15fcdabf9c8265d312aab2e672bad77479ea0623
brunoais <brunoaiss@gmail.com>
2016-10-09 12:57:33 +0100

    Better default for triple callback

--- src/main/java/pt/brunoais/cli_args_parser/callbacks/ValCallback.java
+++ src/main/java/pt/brunoais/cli_args_parser/callbacks/ValCallback.java
@@ -3,6 +3,6 @@ package pt.brunoais.cli_args_parser.callbacks;
 public interface ValCallback {
 	void c(String val1, String val2, String val3);
 	default void c(String val1, String val2, String val3, String val4){
-		c(val1, val2, val3);
+		c(val2, val3, val4);
 	}
 }
