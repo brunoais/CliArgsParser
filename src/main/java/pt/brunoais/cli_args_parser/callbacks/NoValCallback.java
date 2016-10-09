1976e192162eb45a9f4100ef44e261f16df4b5bd
brunoais <brunoaiss@gmail.com>
2016-10-09 12:47:54 +0100

    Missing default method

--- src/main/java/pt/brunoais/cli_args_parser/callbacks/NoValCallback.java
+++ src/main/java/pt/brunoais/cli_args_parser/callbacks/NoValCallback.java
@@ -2,7 +2,9 @@ package pt.brunoais.cli_args_parser.callbacks;
 
 @FunctionalInterface
 public interface NoValCallback extends ValCallback{
-	default void c(String val1, String val2, String val3){}
+	default void c(String val1, String val2, String val3){
+		callback();
+	}
 	
 	void callback();
 }
